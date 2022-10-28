8d991a702c8cb50c17cbfb64eec4b4e246d27480
ajaysinghpanwar2002 <ajaysingh07032003@gmail.com>
2022-10-28 14:44:01 +0530

    basic app setup using chakra UI

---
+++ chakra-ui-videoapp/src/App.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+const App = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default App

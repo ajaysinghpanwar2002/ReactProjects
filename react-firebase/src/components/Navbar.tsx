3a2033e4c5ead99d418dcccfd63c9920cb8922a4
ajaysinghpanwar2002 <ajaysingh07032003@gmail.com>
2022-11-10 11:26:15 +0530

    google authentication, login page done

--- react-firebase/src/components/Navbar.tsx
+++ react-firebase/src/components/Navbar.tsx
@@ -1,18 +1,27 @@
 import React from 'react'
 import { Link } from "react-router-dom"
-import {auth} from '../config/firebase'
-import {useAuthState} from 'react-firebase-hooks/auth'
+import { auth } from '../config/firebase'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { signOut } from "firebase/auth"
 
 function Navbar() {
 
-    const[user] = useAuthState(auth);
+    const signUserOut = async () => {
+        await signOut(auth);
+    }
+    const [user] = useAuthState(auth);
     return (
         <div>
             <Link to="/">Home</Link>
             <Link to="/login">Login</Link>
             <div>
-                <p> {user?.displayName} </p>
-                <img src={user?.photoURL  || ""} width="100" height="100"/>
+                {user && (
+                    <>
+                        <p> {user?.displayName} </p>
+                        <img src={user?.photoURL || ""} alt="ajay" width="100" height="100" border-radius = "50%"/>
+                        <button onClick={signUserOut}>Sign Out</button>
+                    </>
+                )}
             </div>
         </div>
     )

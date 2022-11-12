026d1976f121da2596ff874b4d261d03dd0770e8
ajaysinghpanwar2002 <ajaysingh07032003@gmail.com>
2022-11-12 11:03:51 +0530

    majo part done

--- react-firebase/src/pages/main/Main.tsx
+++ react-firebase/src/pages/main/Main.tsx
@@ -3,13 +3,12 @@ import { getDocs, collection } from "firebase/firestore"
 import { db } from '../../config/firebase'
 import Post from './Post';
 
-interface Post {
+export interface Post {
   id: string;
   userId: string;
   title: string;
   username: string;
   description: string;
-
 }
 
 function Main() {
@@ -23,12 +22,14 @@ function Main() {
     );
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     getPosts();
-  },[])
+  }, [])
   return (
     <div>
-      {postsList?.map((post) =>(<Post/>))}
+      {postsList?.map((post) => (
+        <Post post={post} />)
+      )}
     </div>
   )
 }
--- react-firebase/src/pages/main/Post.tsx
+++ react-firebase/src/pages/main/Post.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import {Post as IPost} from "./Main";
 
-function Post() {
-  return (
-    <div>post</div>
-  )
+interface Props {
+    post: IPost;
+}
+
+function Post(props: Props) {
+    const {post} = props;
+    return (
+        <div>
+            <div className="title">
+                <h1>{post.title}</h1>
+            </div>
+            <div className="body">
+                <p>{post.description}</p>
+            </div>
+            <div className="footer">
+                <p>@{post.username}</p>
+                <button>&#128077;</button>
+            </div>
+        </div>
+    )
 }
 
 export default Post
